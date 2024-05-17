a18ea74940bb5884dd1900a938c71571af506516
Mrzin Ofc <145413073+mrzinofc@users.noreply.github.com>
2024-05-17 17:01:52 -0300

    Barra-de-Nav-JS

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const hamburgerButton = document.querySelector('#hamburgerButton')
+const closeButton = document.querySelector('#closeButton')
+
+const mobileMenu = document.querySelector('#mobileMenu')
+
+hamburgerButton.addEventListener('click', function() {
+    mobileMenu.classList.add('flex');
+});
+
+closeButton.addEventListener('click', function() {
+    mobileMenu.classList.remove('flex') ;   
+});
